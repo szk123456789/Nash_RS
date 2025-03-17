89941779d1da55b7ea515ea653d721c3fb30198a
szk123456789 <95685666+szk123456789@users.noreply.github.com>
2025-03-17 10:40:47 +0800

    main code: nash_rs

---
+++ nash_main.sh
@@ -0,0 +1,41 @@
+set -x 
+
+read -r -d '' training_commands <<EOF
+/root/nash_main.py \
+   --pretrain  /root/.cache/huggingface/hub/models--meta-llama--Llama-3.2-1B-Instruct\
+   --reward_pretrain OpenRLHF/Llama-3-8b-rm-mixture \
+   --save_path /root/autodl-tmp/ckpt/models_NashRS_512prompt_trivial-12th \
+   --ckpt_path /root/autodl-tmp/ckpt/checkpoints_NashRS_512prompt_trivial-12th
+   --save_steps -1 \
+   --logging_steps 1 \
+   --eval_steps -1 \
+   --micro_train_batch_size 4 \
+   --train_batch_size 128 \
+   --micro_rollout_batch_size 4 \
+   --rollout_batch_size 1024 \
+   --max_epochs 1 \
+   --prompt_max_len 1024 \
+   --generate_max_len 1024 \
+   --zero_stage 2 \
+   --bf16 \
+   --actor_learning_rate 5e-6 \
+   --critic_learning_rate 9e-5 \
+   --init_kl_coef 0.18 \
+   --prompt_data OpenRLHF/prompt-collection-v0.1 \
+   --input_key context_messages \
+   --apply_chat_template \
+   --max_samples 100000 \
+   --normalize_reward \
+   --adam_offload \
+   --flash_attn \
+   --gradient_checkpointing \
+   --actor_init_on_gpu \
+   --actor_batch_size 1 \
+   --rejection_batch_size 1 \
+   --truncate_steps 512
+EOF
+    # --wandb [WANDB_TOKENS] or True (use wandb login command)
+
+if [[ ${1} != "slurm" ]]; then
+    deepspeed $training_commands
+fi
