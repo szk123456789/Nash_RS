cb1587e56f11e8b0d6771a270a02fc82e37b46ee
szk123456789 <95685666+szk123456789@users.noreply.github.com>
2025-03-18 11:18:02 +0800

    Update code-base

--- run_eval_reward_openrlhf.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-bash /root/eval_reward_openrlhf.sh /root/.cache/huggingface/hub/models_OnlineIPO_512prompt_trivial-3th /root/autodl-tmp/ckpt/Eval_openrlhf_Llama-3.2-1B-Instruct_OnlineIPO_3 512 128 128
-bash /root/eval_reward_openrlhf.sh /root/.cache/huggingface/hub/models_OnlineIPO_512prompt_trivial-4th /root/autodl-tmp/ckpt/Eval_openrlhf_Llama-3.2-1B-Instruct_OnlineIPO_4 512 128 128
